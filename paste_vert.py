1309d4130ede538ce7c49c3b8b017cb6769215aa
Rév O'Conner <88772846+revoconner@users.noreply.github.com>
2022-06-24 04:12:37 +0530

    Create paste_vert.py

---
+++ paste_vert.py
@@ -0,0 +1,8 @@
+from maya import cmds
+import maya.api.OpenMaya as om
+sel = cmds.ls(sl=True)
+selection_list = om.MSelectionList ()
+selection_list.add(sel[0])
+dag_path = selection_list.getDagPath (0)
+mfn_mesh = om.MFnMesh(dag_path)
+mfn_mesh.setPoints(points)

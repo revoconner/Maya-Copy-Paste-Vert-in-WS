82700b14ea68541f7dab7cfdef9e6dd0e5abe512
Rév O'Conner <88772846+revoconner@users.noreply.github.com>
2022-12-28 17:26:20 +0530

    Delete copy_vert.py

--- copy_vert.py
+++
@@ -1,8 +0,0 @@
-from maya import cmds
-import maya.api.OpenMaya as om
-sel = cmds.ls(sl=True)
-selection_list = om.MSelectionList ()
-selection_list.add(sel[0])
-dag_path = selection_list.getDagPath (0)
-mfn_mesh = om.MFnMesh(dag_path)
-points = mfn_mesh.getPoints()
